4f90a3bde0d472f1b35399715e2dc7a56a466dfe
kitoshitachi <19521204@gm.uit.edu.vn>
2024-06-05 13:52:44 +0700

    update metrics

--- scripts/Span_detector/utils/eval_utils.py
+++ scripts/Span_detector/utils/eval_utils.py
@@ -30,13 +30,11 @@ def _ready_for_metrics(predictions, labels):
 
 def _compute_metrics(preds, golds) -> tuple[dict]:
     tp = fp = fn = .0
-    n_total = 0
 
     for pred, gold in zip(preds, golds):
         gold_set = set(gold)
         pred_set = set(pred)
         
-        n_total += len(gold_set)
         tp += len(gold_set & pred_set)  # True Positives
         fn += len(gold_set - pred_set)  # False Negatives
         fp += len(pred_set - gold_set)  # False Positives
@@ -44,8 +42,7 @@ def _compute_metrics(preds, golds) -> tuple[dict]:
     precision = 0 if tp + fp == 0 else 1.*tp / (tp + fp)
     recall = 0 if tp + fn == 0 else 1.*tp / (tp + fn)
     f1 = 0 if precision + recall == 0 else 2 * (precision * recall) / (precision + recall)
-    acc = tp / n_total
-    return {'precision': precision, 'recall': recall, 'f1': f1, "acc": acc}
+    return {'precision': precision, 'recall': recall, 'f1': f1}
 
 def compute_metrics(p):
     
